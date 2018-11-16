bf61c6c2d1e50128f52f1240b88869538829a5a6
Viviana Villalobos <vivivillalobos79@hotmail.com>
2018-11-15 19:40:49 -0600

    Update Student
    Update

---
+++ StudentApp/app/src/main/java/tec/datos1/studentapp/MainActivity.java
@@ -0,0 +1,20 @@
+package tec.datos1.studentapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void maps(View view){
+        Intent intent = new Intent(this, Map2.class);
+        startActivity(intent);
+    }
+}

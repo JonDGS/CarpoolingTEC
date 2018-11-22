49b6a79e003f0980267dc81a6905f4a121dcb6f5
Viviana Villalobos <vivivillalobos79@hotmail.com>
2018-11-22 13:10:52 -0600

    Update StartTrip.java

--- DriverApp/app/src/main/java/com/carpoolingtec/davidqs96/driverapp/StartTrip.java
+++ DriverApp/app/src/main/java/com/carpoolingtec/davidqs96/driverapp/StartTrip.java
@@ -1,7 +1,9 @@
 package com.carpoolingtec.davidqs96.driverapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StartTrip extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class StartTrip extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_trip);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message1 = intent.getStringExtra(NewTripScreen.EXTRA_MESSAGE1);
+        String message2 = intent.getStringExtra(NewTripScreen.EXTRA_MESSAGE2);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView txtInicio = findViewById(R.id.txtInicio);
+        txtInicio.setText(message1);
+
+        TextView txtFin = findViewById(R.id.txtDestino);
+        txtFin.setText(message2);
+
+
+
     }
 }
